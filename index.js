03b8f9d47e53b4f67d302acdd85717efdf4e8ddc
Anil Bikram Singh <sing11.anill@gmail.com>
2026-01-29 01:37:44 +0545

    fix: allow cache-control header in CORS

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
 };
 
 app.use(cors(corsOptions));
